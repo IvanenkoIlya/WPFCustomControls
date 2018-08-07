304478cf90750ca29549d715fb01a72c9962adb0
Ilya Ivanenko <IIVanenko@esd.wa.gov>
2018-08-07 08:39:14 -0700

    Added MarqueeLabel and test project

---
+++ MarqueeTextBox/MarqueeLabel.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace MarqueeTextBox
+{
+    [TemplatePart(Name = "PART_Content", Type = typeof(ContentPresenter))]
+    public class MarqueeLabel : Label
+    {
+        #region Dependancy Properties
+        #region LoopMode
+        public enum Loop
+        {
+            FadeToBeginning,
+            Continuous
+        }
+
+        public static readonly DependencyProperty LoopModeProperty =
+            DependencyProperty.Register("LoopMode", typeof(Loop), typeof(MarqueeLabel),
+                new FrameworkPropertyMetadata(Loop.FadeToBeginning));
+
+        public Loop LoopMode
+        {
+            get { return (Loop) GetValue(LoopModeProperty); }
+            set { SetValue(LoopModeProperty, value); }
+        }
+        #endregion
+
+        #region ScrollSpeed
+        public static readonly DependencyProperty ScrollSpeedProperty =
+            DependencyProperty.Register("ScrollSpeed", typeof(double), typeof(MarqueeLabel),
+                new PropertyMetadata(1.0));
+
+        public double ScrollSpeed
+        {
+            get { return (double)GetValue(ScrollSpeedProperty); }
+            set { SetValue(ScrollSpeedProperty, value); }
+        }
+        #endregion
+
+        #region StartPauseTime
+        public static readonly DependencyProperty StartPauseTimeProperty =
+            DependencyProperty.Register("StartPauseTime", typeof(TimeSpan), typeof(MarqueeLabel),
+                new PropertyMetadata(TimeSpan.FromMilliseconds(500)));
+
+        public TimeSpan StartPauseTime
+        {
+            get { return (TimeSpan)GetValue(StartPauseTimeProperty); }
+            set { SetValue(StartPauseTimeProperty, value); }
+        }
+        #endregion
+
+        #region FadeTime
+        public static readonly DependencyProperty FadeTimeProperty =
+            DependencyProperty.Register("FadeTime", typeof(TimeSpan), typeof(MarqueeLabel),
+                new PropertyMetadata(TimeSpan.FromMilliseconds(500)));
+
+        public TimeSpan FadeTime
+        {
+            get { return (TimeSpan)GetValue(FadeTimeProperty); }
+            set { SetValue(FadeTimeProperty, value); }
+        }
+        #endregion
+
+        #region EndPauseTime
+        public static readonly DependencyProperty EndPauseTimeProperty =
+            DependencyProperty.Register("EndPauseTime", typeof(TimeSpan), typeof(MarqueeLabel),
+                new PropertyMetadata(TimeSpan.FromMilliseconds(500)));
+
+        public TimeSpan EndPauseTime
+        {
+            get { return (TimeSpan)GetValue(EndPauseTimeProperty); }
+            set { SetValue(EndPauseTimeProperty, value); }
+        }
+        #endregion
+        #endregion
+
+        public Storyboard storyboard;
+        public ContentPresenter contentPresenter;
+        private TextBlock textBlock;
+
+        private double textWidth;
+
+        static MarqueeLabel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MarqueeLabel), new FrameworkPropertyMetadata(typeof(MarqueeLabel)));
+        }
+
+        public MarqueeLabel()
+        {
+            double h = Height;
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (VisualTreeHelper.GetChildrenCount(contentPresenter) > 0)
+            {
+                textBlock = VisualTreeHelper.GetChild(contentPresenter, 0) as TextBlock;
+
+                textWidth = textBlock.ActualWidth;
+            }
+
+            if(textWidth > ActualWidth)
+            {
+                storyboard = CreateMarqueeStoryboard();
+            }
+        }
+
+        private Storyboard CreateMarqueeStoryboard()
+        {
+            ThicknessAnimationUsingKeyFrames movementAnimation = new ThicknessAnimationUsingKeyFrames();
+            DoubleAnimationUsingKeyFrames opacityAnimation = new DoubleAnimationUsingKeyFrames();
+            double time = StartPauseTime.TotalMilliseconds;
+
+            movementAnimation.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0),
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            time += (textWidth - ActualWidth) / (0.05 * ScrollSpeed);
+
+            movementAnimation.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0) { Left = -textWidth - Padding.Left * 2 - Padding.Right + ActualWidth },
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            time += EndPauseTime.TotalMilliseconds;
+
+            movementAnimation.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0) { Left = -textWidth - Padding.Left * 2 - Padding.Right + ActualWidth },
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+            opacityAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(1.0,
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            time += FadeTime.TotalMilliseconds;
+
+            opacityAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(0.0,
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+            movementAnimation.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0) { Left = -textWidth - Padding.Left * 2 - Padding.Right + ActualWidth },
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            time += 1;
+
+            movementAnimation.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0),
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            time += FadeTime.TotalMilliseconds;
+
+            opacityAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(1.0,
+                KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(time))));
+
+            Storyboard storyboard = new Storyboard
+            {
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            Storyboard.SetTarget(movementAnimation, textBlock);
+            Storyboard.SetTarget(opacityAnimation, textBlock);
+            Storyboard.SetTargetProperty(movementAnimation, new PropertyPath(MarginProperty));
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(OpacityProperty));
+
+            storyboard.Children.Add(movementAnimation);
+            storyboard.Children.Add(opacityAnimation);
+
+            return storyboard;
+        }
+
+        public void PlayAnimation()
+        {
+            storyboard.Begin();
+        }
+
+        public void StopAnimation()
+        {
+            storyboard.Stop();
+        }
+
+        public void PauseAnimation()
+        {
+            storyboard.Pause();
+        }
+
+        public void ResumeAnimation()
+        {
+            storyboard.Resume();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            contentPresenter = GetTemplateChild("PART_Content") as ContentPresenter;
+        }
+    }
+}
---
+++ MarqueeTextBoxTest/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarqueeTextBoxTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick_Play(object sender, RoutedEventArgs e)
+        {
+            TestMarquee.PlayAnimation();
+        }
+
+        private void OnClick_Stop(object sender, RoutedEventArgs e)
+        {
+            TestMarquee.StopAnimation();
+        }
+
+        private void OnClick_PauseResume(object sender, RoutedEventArgs e)
+        {
+            if(TestMarquee.storyboard.GetIsPaused())
+            {
+                PauseResumeButton.Content = "Pause";
+                TestMarquee.ResumeAnimation();
+            }
+            else
+            {
+                PauseResumeButton.Content = "Resume";
+                TestMarquee.PauseAnimation();
+            }
+        }
+    }
+}
